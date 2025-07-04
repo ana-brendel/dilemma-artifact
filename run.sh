5b4c37d7740d302d8547749e7b26e48f2f9dce62
Ana Brendel <anabrendel25@gmail.com>
2025-07-04 16:06:31 -0700

    update script for updated group tag

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == "group" ]; then
+if [ $1 == "group" || $1 == "group_updated"]; then
       python3 testing_scripts/run.py $1 $2
     else
       python3 testing_scripts/run.py $1
