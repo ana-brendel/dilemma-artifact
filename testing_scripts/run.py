2c1ff3a77fe8cfd23656300fc83706759806c92a
Ana Brendel <anabrendel25@gmail.com>
2025-07-18 15:21:40 -0700

    fix script

--- testing_scripts/run.py
+++ testing_scripts/run.py
@@ -122,7 +122,7 @@ def lfind_target(test):
 def run_lfind_group(group):
     tests = group_lfind_tests[group]
     for t in tests:
-        r = run_test(t)
+        r = run_lfind_test(t)
         target = lfind_target(t)
         results += [(t,target,r)]
     return results
